ecf1d815663cb030ca57073f39bc6ac3983aa3df
abbiemurugi <abigaelmurugi@gmail.com>
2018-07-13 19:38:31 +0300

    index plus script.js changes

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$("form#abby").click(function(event){
+    event.preventDefault();
+    for(i=1;i<=$("input").val();i++) {
+       // $("ul#list").append("<li>"+  i + "</li>");//
+        if((i%3===0)&&(i%5===0)){$("ul#list").append("<li> pingpong</li>");}
+    else if(i%3===0){$("ul#list").append("<li>pong</li>");}
+else if(i%5===0){$("ul#list").append("<li> ping </li>");}
+else {$("ul#list").append("<li>" + i + "</li>");}
+    };
+
+
+
+
+});
+});
+
+
+
